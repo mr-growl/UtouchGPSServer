afad14c5ce1029b299b9721515422826a923ad55
Andrew Bates <ahmbates@gmail.com>
2019-10-18 22:08:41 +1000

    Added some threading.  Now the server and gps are in separate threads.  The gps will run while requests are coming in and will keep running for a further 5 minutes just to keep things warm.
    Need to add comments

--- track_attack_client.py
+++ track_attack_client.py
@@ -11,15 +11,23 @@ try:
 		s.sendall(b'supersneakylocation')
 		data = s.recv(1024)
 		print('Received', repr(data))
-		elevation, longitude, latitude = [i for i in data.decode('utf-8').split('\n')]
-		if elevation == 'invalid':
-			print("INVALID LOCATION")
-		elif elevation == 'bad secret':
-			print("BAD SECRET")
-		else:
+		resp = data.decode('utf-8');
+		respcount = len(resp.split('\n'))
+		if respcount == 3:
+			#elevation, longitude, latitude = [i for i in resp.split('\n')]
+			elevation, longitude, latitude = [i for i in data.decode('utf-8').split('\n')]
 			elevation = int(elevation)
 			print("Elevation: "+str(elevation))
 			print("Longitude: "+longitude)
 			print("Latitude: "+latitude)
+		elif respcount == 1:
+			if resp == 'invalid':
+				print("INVALID LOCATION")
+			elif resp == 'bad secret':
+				print("BAD SECRET")
+			else:
+				print("NOPE 1")
+		else:
+			print("NOPE")
 except:
 	print("Caught exception while connecting to server")
--- track_attack_server.py
+++ track_attack_server.py
@@ -33,9 +33,11 @@
 # 0.2 update UI
 
 import subprocess
+import threading
 import re
 import sys
 import socket
+import time
 
 import math as mod_math
 
@@ -45,110 +47,148 @@ socket_address = '127.0.0.1'
 socket_port = 61234
 socket_secret = b'supersneakylocation'
 
-location_valid = False
+
+last_request_seconds = 0
+request_runout_seconds = 300
+current_location = str.encode("invalid")
+
+debug = True
 
 s = None
 
 def read_gps():
-	retry_limit = 10
-	retry_count = 0
-
 	global latitude
 	global longitude
 	global accuracy
 	global elevation
-	global location_valid
+	global last_request_seconds
+	global request_runout_seconds
+	global current_location
+
 	latitude = str("")
 	longitude = str("")
 	elevation = str("")
 	accuracy = str("")
 	elevation_a = []
+
+	location_expiry_seconds = 10
+	last_valid_location_seconds = 0
+
 	last_frame_good = False
+	running = False
 
 	cmd = ['sudo test_gps']
-	p = subprocess.Popen(cmd, stdout = subprocess.PIPE,
-	stderr = subprocess.STDOUT, shell = True)
-	for line in p.stdout:
-		line = line.decode('utf-8')
-		# search for "*** sv status" to signal start of new frame"
-		if re.search("^\*\*\* sv status", line):
-				print("START OF NEW FRAME")
-				retry_count = retry_count + 1
-				if retry_count > retry_limit:
-					print("MAX RETRYS HIT... LEAVING")
-					location_valid = False
-					p.kill()
-					return elevation,longitude,latitude
-					
-				#if the last frame ended without an accuracy value then set the current location as not valid
-				if last_frame_good is False:
-					    location_valid = False
-				#reset the 
-				last_frame_good = False
 
-		if re.search("^latitude", line):
-				latitude = line.split()
-				latitude = latitude[1]
-
-		if re.search("^longtide", line): #bug in test_gps
-				longitude = line.split()
-				longitude = longitude[1]
-
-		if re.search("^accuracy", line): #bug in test_gps
-				accuracy = line.split()
-				accuracy= accuracy[1]
-
-		if re.search("elevation", line): #bug in test_gps
-				elevation_T = line.split()
-				#print(elevation)
-				elevation_a.append(elevation_T[1])
-				#print(elevation)
-
-		if accuracy != "":
-			location_valid = True
-			print("GOOD FRAME!!!")
-			#print(line + "=> accuracy : "  + accuracy)
-			#print(line + "=> longitude : "  + longitude)
-			#print(line + "=> latitude : "  + latitude)
-			for i in range(len(elevation_a)):
-				elevation_a[i] = float(elevation_a[i])
-			#elevation = int(sum(elevation_a) / float(len(elevation_a)))
-			elevation = str(int(sum(elevation_a) / float(len(elevation_a))))
-			p.kill()
-			return elevation,longitude,latitude
-
-try:
 	while True:
-		with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-			s.bind((socket_address, socket_port))
-			s.listen()
-			conn, addr = s.accept()
-			with conn:
-				print('Connected by', addr)
-				while True:
-					data = conn.recv(1024)
-					if not data:
-						break
-					#check if the secret was sent
-					if socket_secret == data:
-						print("SECRET ACCEPTED")
-						read_gps()
-						if location_valid is True:
-							print("SENDING GOOD LOCATION")
-							print(type(elevation))
-							print(type(longitude))
-							print(type(latitude))
-							conn.sendall(str.encode("\n".join([elevation,longitude,latitude])))
-						else:
-							print("SENDING BAD LOCATION")
-							conn.sendall(str.encode("\n".join(["invalid","invalid","invalid"])))
-					else:
-						print("BAD SECRET")
-						conn.sendall(str.encode("\n".join(["bad secret","bad secret","bad secret"])))
+		if running is False:
+			if int(time.time()) - last_request_seconds < request_runout_seconds:
+				if debug:
+					print("GPS SERVER WAKING UP")
+				p = subprocess.Popen(cmd, stdout = subprocess.PIPE,
+				stderr = subprocess.STDOUT, shell = True)
+				running = True
+			else:
+				time.sleep(1)
+		if running is True:
+			for line in p.stdout:
+				line = line.decode('utf-8')
+				# search for "*** sv status" to signal start of new frame"
+				if re.search("^\*\*\* sv status", line):
+					if debug:
+						print("START OF NEW FRAME")
+							
+					#if the last frame ended without an accuracy value then set the current location as not valid
+					if last_frame_good is False:
+						if int(time.time()) - last_valid_location_seconds > location_expiry_seconds:
+							current_location = str.encode("invalid")
+					#reset the 
+					last_frame_good = False
+					if int(time.time()) - last_request_seconds > request_runout_seconds:
+						if debug:
+							print("GPS SERVER GOING TO SLEEP")
+						running = False
+						p.kill()
+						current_location = str.encode("invalid")
+						break;
+		
+				if re.search("^latitude", line):
+						latitude = line.split()
+						latitude = latitude[1]
+		
+				if re.search("^longtide", line): #bug in test_gps
+						longitude = line.split()
+						longitude = longitude[1]
+		
+				if re.search("^accuracy", line): #bug in test_gps
+						accuracy = line.split()
+						accuracy= accuracy[1]
+		
+				if re.search("elevation", line): #bug in test_gps
+						elevation_T = line.split()
+						#print(elevation)
+						elevation_a.append(elevation_T[1])
+						#print(elevation)
+	
+				if accuracy != "":
+					last_frame_good = True
+					last_valid_location_seconds = int(time.time())
+					if debug:
+						print("GOOD FRAME!!!")
+					for i in range(len(elevation_a)):
+						elevation_a[i] = float(elevation_a[i])
+					elevation = str(int(sum(elevation_a) / float(len(elevation_a))))
+					current_location = str.encode("\n".join([elevation,longitude,latitude]))
+					accuracy = ""
 
-except KeyboardInterrupt:
+
+
+
+if __name__ == "__main__":
+	location_thread = threading.Thread(target=read_gps, daemon=True)
+	location_thread.start()
 	try:
-		s.close()
-	except:
-		print("Problem closing socket")
-	sys.exit()
+		while True:
+			with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+				s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+				try:
+					s.bind((socket_address, socket_port))
+					s.listen()
+					conn, addr = s.accept()
+					with conn:
+						if debug:
+							print('Connected by', addr)
+						while True:
+							data = conn.recv(1024)
+							if not data:
+								break
+							#check if the secret was sent
+							if socket_secret == data:
+								if debug:
+									print("SECRET ACCEPTED")
+								last_request_seconds = int(time.time())
+								conn.sendall(current_location)
+							else:
+								if debug:
+									print("BAD SECRET")
+								conn.sendall(str.encode("bad secret"))
+					s.close()
+				except KeyboardInterrupt:
+					try:
+						s.close()
+					except:
+						if debug:
+							print("Problem closing socket")
+					sys.exit()
+				except:
+					dummy = True
+					#if debug:
+						#print("Problem Binding Server")
+	
+	
+	except KeyboardInterrupt:
+		try:
+			s.close()
+		except:
+			if debug:
+				print("Problem closing socket")
+		sys.exit()
